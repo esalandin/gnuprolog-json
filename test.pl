7e0cb8bebde42aaff16bda5701783439f2e340ef
Salandin, Emanuele <esalandin@alice.it>
2018-03-04 19:43:44 +0100

    setup environment

---
+++ test.pl
@@ -0,0 +1,10 @@
+%% gprolog --consult-file test.pl --entry-goal test
+
+:- include(json).
+
+test(A):- A, !; print(A), print(' failed'),nl,fail.
+
+test_json_file(JF, JS):- json_decode(JF, JO), json_encode(JO, S), atom_codes(JS, S).
+
+test:- test(test_json_file('t1.json', _)), test(test_json_file('t2.json', _)), test(test_json_file('e1.json', _)), test(test_json_file('e2.json', _)), test(test_json_file('e3.json', _)).
+
